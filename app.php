272c02b3bf26427bb7bdd2127d79aeb9b183af58
Abid Rahman Shafi <69119769+ashafio@users.noreply.github.com>
2023-05-04 01:26:59 +0600

    Add files via upload

---
+++ app.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>A Web Page</title>
+</head>
+<body>
+
+<h1>
+    <?php
+        echo "Let's Start Again... Let's Build Something! :)"
+    ?>
+</h1>
+
+<p>
+        
+<?php
+ echo "This is a basic PHP document";
+?>
+            
+</p>
+
+<p>
+
+<?
+ print "PHP is fun!";
+?>
+
+</p>
+
+<p>
+        
+<script language="php">
+ $myVar = "Hello World";
+echo $myVar;  
+
+</script>
+            
+</p>
+
+</body>
+</html>
+         
